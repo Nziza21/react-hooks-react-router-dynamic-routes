bdb450952ccada6174560eb0944951ef7f329257
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-11-04 19:35:55 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,27 @@ import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
 function App() {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "A River Runs Through It" },
-    { id: 2, title: "Se7en" },
-    { id: 3, title: "Inception" }
-  ]);
+  const [movies] = useState({
+    1: {
+      id: 1,
+      title: "A River Runs Through It",
+      description: "A movie about a family in Montana.",
+      releaseDate: "May 26, 1992",
+    },
+    2: {
+      id: 2,
+      title: "Se7en",
+      description: "A thriller about detectives chasing a serial killer.",
+      releaseDate: "September 22, 1995",
+    },
+    3: {
+      id: 3,
+      title: "Inception",
+      description: "A science fiction heist thriller.",
+      releaseDate: "July 16, 2010",
+    },
+  });
+  
 
   return (
     <div>
